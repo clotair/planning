d5e4d5a805159a1fd111aca64f5623b5d55d40df
tkp_237 <tchuentekammognepatrice@gmail.com>
2019-02-09 00:46:30 +0100

    voila un footer bien fait

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -12,11 +12,16 @@
     @section('menu')
     	<div id="navbar" class="collapse navbar-collapse">
           	<ul class="nav navbar-nav">
-            	<li class="active"><a class = "orientation" href="/"><span class="glyphicon glyphicon-home"></span>&nbsp;Home</a></li>
+            	<li class="active"><a class = "orientation" href="/"><span class="glyphicon glyphicon-home"></span>&nbsp;Accueil</a></li>
             	<li><a class = "orientation" href="/salle"><span class ="glyphicon glyphicon-inbox"></span> &nbsp;Nos Salles</a></li>
-            	<li><a class = "orientation" href="/materiel"><span class ="glyphicon glyphicon-scissors"></span> &nbsp;Materiels</a></li>
+            	<li><a class = "orientation" href="/materiel"><span class ="glyphicon glyphicon-scissors"></span> &nbsp;Matériels</a></li>
             	<li><a class = "orientation" href="/enseignant"><span class ="glyphicon glyphicon-briefcase"></span> &nbsp;Enseignant</a></li>
-            	<li><a class = "orientation" href="/classe"><span class ="glyphicon glyphicon-briefcase"></span> &nbsp;Classe</a></li>
+				<li><a class = "orientation" href="/classe"><span class ="glyphicon glyphicon-briefcase"></span> &nbsp;Classe</a></li>
+				<li><a class = "orientation" href="#"><span class ="glyphicon glyphicon-briefcase"></span> &nbsp;Evènement</a></li>
+				
+				
+				<li>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class = "orientation" href="#"><span class ="glyphicon glyphicon-briefcase"></span> &nbsp;Connexion</a></li>
           	</ul>
         </div><!--/.nav-collapse -->
      
