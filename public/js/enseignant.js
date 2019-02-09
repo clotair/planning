88d1077d69b640e7b01b368715ea2762c391f9ac
fez2000 <eugenefezeu@gmail.com>
2019-02-09 03:24:31 +0100

    recherche

--- public/js/enseignant.js
+++ public/js/enseignant.js
@@ -1,10 +1,115 @@
+
+
+let prev_mot = '';
+let prev_tab = [];
 $(function () {
-  $('[data-toggle="popover"]').popover()
+  $('[data-toggle="popover"]').popover();
 })
-function planning_enseignant(e){
- $('.salle.active').removeClass('active');
+function mois_n(n){
+    let mois = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Decembre'];
+    return mois[n];
+}
+function jour(n){
+    let jours = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimache'];
+    return jours[n];
+}
+function sup(){
+    $('.rechercheresult').html('');
+}
+function rien(){
+    $('.rechercheresult').html("<div class=' col-xs-8'><center><h5 class='display-4'>Auqu'un enseignant trouver</h5><center></div>");
+}
+function result(data){
+    let ul = $('<ul/>').addClass('list-group list-group-flush col-xs-8');
+    for(let i of data){
+        $(ul).append(
+            $('<li/>').addClass('list-group-item').append(
+                $('<a/>').attr('href','#e'+i.id).on('click',()=>{
+                    $('#e'+i.id).trigger('click');
+                }).html(i.nom)
+                
+                )
+        )
+    }
+    $('.rechercheresult').html('');
+    $('.rechercheresult').append(ul)
+}
+function search_enseignant(data){
+    let val = $('.terme').val();
+    console.log(data);
+    valupc = val.toUpperCase()
+    let tab = [];
+    if(val >= prev_mot && prev_mot){
+        for(let i of prev_tab){
+            if(i['code'].indexOf(val)!=-1 || i['code'].indexOf(valupc)!=-1){
+                tab.push(i)
+            }else if(i['nom'].indexOf(val)!=-1 || i['nom'].indexOf(valupc)!=-1){
+                tab.push(i)
+            }
+        }
+    }else{
+        for(let i of data){
+            if(i['code'].indexOf(val)!=-1 || i['code'].indexOf(valupc)!=-1){
+                tab.push(i)
+            }else if(i['nom'].indexOf(val)!=-1 || i['nom'].indexOf(valupc)!=-1){
+                tab.push(i)
+            }
+        }
+    }
+    if(tab.length!=0){
+        result(tab);
+    }else{
+        rien();
+    }
+    if(!val){
+        sup()
+    }
+    prev_mot = val;
+    rev_tab = tab;
+    if(!val){
+        prev_tab = data
+    }
+}
+function search_enseignant_active(){
+    sup()
+    $('.calendrier').hide(400);
+    $('.recherche').fadeIn(500); 
+    $('.enseignant.active').removeClass('active');
+    $('.br').addClass('active');
+}
+function get_info(id){
+    $.get({
+        url:'/api/enseignant/'+id,
+        success:(data)=>{
+            if(data.length!=0){
+                let val = data[0];
+                $('#enseignantModal .modal-title').html(val.prof);
+                $('#enseignantModal .modal-body').html(
+                    $('<div/>').addClass('container')
+                    .append(
+                        
+                        $('<div/>').addClass('col-xs-4').html('NOM:')
+                    )    
+                    .append(
+                        $('<div/>').addClass('col-xs-6').html('<b>'+val.prof+'</b>')
+    
+                    ).append(
+                        $('<div/>').addClass('col-xs-4').html('GRADE:')
+                    ).append(
+                        $('<div/>').addClass('col-xs-4').html('<b>'+val.grade+'</b>')
+                    )
+                
+                )
+            }
+        }
+    })
+}
+function planning_enseignant(e,titre){
+ $('.recherche').hide(400);
+ $('.calendrier').fadeIn(500);
+ $('.enseignant.active').removeClass('active');
  $('#e'+e).addClass('active');
- 
+ get_info(e);
  $.get({
      url:'/api/enseignant/planning/'+e,
      success:(data)=>{
@@ -12,17 +117,18 @@ function planning_enseignant(e){
         let tab = [];
         tab.push($('<tr/>').append(
             $('<td/>').css({
-                height:'50px',
-                width:'100px'
+                
+                height:'50px'
+                
             })
         ))
         for(let i = 1;i<=17;i++){
             
             tab.push($('<tr/>').append(
                 $('<td/>').css({
-                                height:'100px',
+                                height:'50px',
                 
-            }).html((i+5)+'h')
+                }).attr('scope','row').html((i+5)+'h')
             ))
 
         }
@@ -30,7 +136,13 @@ function planning_enseignant(e){
 
         for(let i of data){
             let date = new Date( i['date'] );
-            $(tab[0]).append($('<td/>').append(i['date']))
+            $(tab[0]).append($('<th/>').attr('scope','col').css({
+                width: '200px',
+                
+                height: '50px',
+                'text-align': 'center',
+                'padding-top': '2px',
+            }).attr('title',jour(date.getDay())+', le '+date.getDate()+' '+ mois_n(date.getMonth())+date.getFullYear()).append('<p style="width:200px">'+jour(date.getDay())+', le '+date.getDate()+' '+ mois_n(date.getMonth())+'</p>' ))
             for(let j=1;j<=17;j++){
                 for(let y of i['heures']){
                     
@@ -38,15 +150,15 @@ function planning_enseignant(e){
                         
                             $(tab[j]).append(
                                 $('<td/>').css({
-                                    
-                                    height:'100px',
+                                    'font-size': '12px',
+                                    height:'50px',
                                     'background-color':'orange',
-                                    'opacity':'0.8'
+                                    'opacity':'0.8',
+                                    'text-align': 'center'
                                 }).attr('title',y['heure_debut']+'/'+y['heure_fin']).html(
-                                    '<b>'+y['description']['type'][0]['nom']+'</b>'+
-                                   '<br/> <b>'+ y['description']['matiere'][0]['code']+'</b>' +'<br/>'
-                                   +'<br/>'+'<b>' +y['description']['classe'][0]['code']+'</b>'
-                                  
+                                    '<center>'+'<b>'+y['description']['type'][0]['nom']+'</b>'+'<br/> <b>'+ y['description']['matiere'][0]['code']+'</b>'+'<br/>'
+                                   + ' <b>'+y['description']['classe'][0]['code']+'</b>'
+                                   +'</b>'+'</center>'
                                 )
                             )
 
@@ -54,30 +166,35 @@ function planning_enseignant(e){
                     }else{
                         $(tab[j]).append($('<td/>').css({
                             
-                            height:'100px',
+                            height:'50px',
                             
                         }).html('')
-                        );  
+                        );
                     }
                  
-                }    
+                }
+         
                 if(i['heures'].length==0){
                     $(tab[j]).append($('<td/>').css({
                             
-                        height:'100px',
+                        height:'50px',
                         
                     }).html('')
                     );
-                } 
+                }    
             }
             
            
         }
-        for(let i = 0;i<=17;i++){
+        for(let i = 1;i<=17;i++){
            
             $(t).append(tab[i])
         }
-        $('#listePE').html(t)
+        let h = $('<thead/>').append(tab[0]);
+        $('#listePE').html(h);
+        $('#listePE').append(t);
+        $('#titreE').html(titre);
+        
      }
  })
 }
--- resources/views/enseignant/liste.blade.php
+++ resources/views/enseignant/liste.blade.php
@@ -3,65 +3,89 @@
 
 @section('title', 'Enseignants')
 @section('css')
-    <!-- liens css -->
+  	<!-- liens css -->
 @endsection
 @section('sidebar')
     @parent
 
     @section('menu')
         <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                        <li><a class = "orientation" href="/"><span class="glyphicon glyphicon-home"></span>&nbsp;Accueil</a></li>
-                        <li><a class = "orientation" href="/salle"><span class ="glyphicon glyphicon-inbox"></span> &nbsp;Nos Salles</a></li>
-                        <li><a class = "orientation" href="/materiel"><span class ="glyphicon glyphicon-scissors"></span> &nbsp;Matériels</a></li>
-                        <li class="active"><a class = "orientation" href="/enseignant"><span class ="glyphicon glyphicon-briefcase"></span> &nbsp;Enseignant</a></li>
-                        <li><a class = "orientation" href="/classe"><span class ="glyphicon glyphicon-book"></span> &nbsp;Classe</a></li>
-                        <li><a class = "orientation" href="/evenement"><span class ="glyphicon glyphicon-bell"></span> &nbsp;Evènement</a></li>
-                        
-                        
-                        <li>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class = "orientation" href="/admin" target ="_blank"><span class ="glyphicon glyphicon-user"></span> &nbsp;Connexion</a></li>
-                      </ul>
+				<ul class="nav navbar-nav">
+						<li><a class = "orientation" href="/"><span class="glyphicon glyphicon-home"></span>&nbsp;Accueil</a></li>
+						<li><a class = "orientation" href="/salle"><span class ="glyphicon glyphicon-inbox"></span> &nbsp;Nos Salles</a></li>
+						<li><a class = "orientation" href="/materiel"><span class ="glyphicon glyphicon-scissors"></span> &nbsp;Matériels</a></li>
+						<li class="active"><a class = "orientation" href="/enseignant"><span class ="glyphicon glyphicon-briefcase"></span> &nbsp;Enseignant</a></li>
+						<li><a class = "orientation" href="/classe"><span class ="glyphicon glyphicon-book"></span> &nbsp;Classe</a></li>
+						<li><a class = "orientation" href="/evenement"><span class ="glyphicon glyphicon-bell"></span> &nbsp;Evènement</a></li>
+						
+						
+						<li>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class = "orientation" href="/admin" target ="_blank"><span class ="glyphicon glyphicon-user"></span> &nbsp;Connexion</a></li>
+					  </ul>
         </div><!--/.nav-collapse -->
      
     @endsection
 @endsection
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="list-group">
-                    <button type="button" class="list-group-item list-group-item-action active salle" onClick="search_enseignant({{$enseignants}})">
-                        Rechercher
-                    </button>
+
+<div class="container" style="margin-top:70px;">
+  <div class="row">
+    <div class="col-md-3">
+      <div class="jolieliste">
+        <div class="list-group" style="text-align:center;">
+            <button type="button" class="list-group-item list-group-item-action active enseignant br" onClick="search_enseignant_active()" style="text-align:center">
+            RECHERCHER
+            </button>
       
-
-
-                    @foreach($enseignants as $enseignant)
+            			@foreach($enseignants as $enseignant)
  
-                        <button type="button"  id="{{'e'.$enseignant->id}}" class="list-group-item list-group-item-action salle" onClick="planning_enseignant({{$enseignant->id}})" data-toggle="popover"  data-content="{{$enseignant->prof}}" title="{{$enseignant->grade}}"> 
-                            {{$enseignant->prof}}
-                        </button>
-                    @endforeach
-                </div>
-            </div>
-            <div class="col-md-9">
-                <div class="table-responsive">
-                    <table class="table" id="listePE">
+              				<button type="button"  id="{{'e'.$enseignant->id}}" class="list-group-item list-group-item-action classe" onClick="planning_enseignant({{$enseignant->id}},'{{$enseignant->prof}}')"    title="{{$enseignant->prof}}"> 
+               				 	{{$enseignant->prof}}
+              				</button>
+            			@endforeach
+        			</div>
+      			</div>
+    		</div>
+    		<div class="col-md-9 recherche">
+      			<form>
+       				<center><h4 >RECHERCHE</h4></center><br/>
+        			<div class="col-md-2"></div>
+        			<div class="col-md-9 ">
+          				<input type="search" class="form-control"  placeholder="Entrez le nom d'un enseignant" onInput="search_enseignant({{$enseignants}})"/>
+					</div>
+				</form>
+    		</div>
+    		<div class="col-md-9 calendrier">
+      			<center><a href="#enseignantModal" data-toggle="modal" data-target="#enseignantModal"><h4 id="titreE"></h4></a></center><br/>
+    			<div class="table-responsive">
+      				<table class="table table-bordered" id="listePE">
     
-                    </table>
-                </div>
-            </div>
-        </div>
+      				</table>
+    			</div>
+    		</div>
+    	</div>
+	</div>
+  <div class="modal fade bd-example-modal-xl" tabindex="-1" id="enseignantModal" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+    <div class="modal-header" style="text-align:center;">
+        <strong class="modal-title" style="font-size: 30px;"></strong>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+ 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+       
+      </div>
     </div>
-
+  </div>
+</div>
 @endsection
 @section('js')
-    <!-- liens js -->
-    <script src="js/enseignant.js"></script>
+  	<!-- liens js -->
+  	<script src="js/enseignant.js"></script>
 @endsection
-
-
-
-
-  
