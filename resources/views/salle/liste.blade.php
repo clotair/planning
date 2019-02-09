6bbdabd5bf59dffbdf0ecd18b8a27f15c0e5c78b
tkp_237 <tchuentekammognepatrice@gmail.com>
2019-02-09 03:52:37 +0100

    voila un footer bien fait

--- resources/views/salle/liste.blade.php
+++ resources/views/salle/liste.blade.php
@@ -12,10 +12,10 @@
         <div id="navbar" class="collapse navbar-collapse">
 				<ul class="nav navbar-nav">
 						<li><a class = "orientation" href="/"><span class="glyphicon glyphicon-home"></span>&nbsp;Accueil</a></li>
-						<li><a class = "orientation" href="/salle"><span class ="glyphicon glyphicon-inbox"></span> &nbsp;Nos Salles</a></li>
+						<li   class="active"><a class = "orientation" href="/salle"><span class ="glyphicon glyphicon-inbox"></span> &nbsp;Nos Salles</a></li>
 						<li><a class = "orientation" href="/materiel"><span class ="glyphicon glyphicon-scissors"></span> &nbsp;Matériels</a></li>
 						<li><a class = "orientation" href="/enseignant"><span class ="glyphicon glyphicon-briefcase"></span> &nbsp;Enseignant</a></li>
-						<li  class="active"><a class = "orientation" href="/classe"><span class ="glyphicon glyphicon-book"></span> &nbsp;Classe</a></li>
+						<li><a class = "orientation" href="/classe"><span class ="glyphicon glyphicon-book"></span> &nbsp;Classe</a></li>
 						<li><a class = "orientation" href="/evenement"><span class ="glyphicon glyphicon-bell"></span> &nbsp;Evènement</a></li>
 						
 						
